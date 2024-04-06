75e4bcb16b5585d178d70a742560c1e61a0d4c4e
ManuelMares <fmms@outlook.com>
2024-04-06 11:33:09 -0600

    v2

--- main.js
+++ main.js
@@ -261,7 +261,6 @@ function bfs(graph, startNode, targetNode, position) {
         }
 
     }
-
     return null; // Path not found
 }
 
@@ -291,3 +290,77 @@ console.log("Optimal Path:", path);
 
 
 
+
+
+
+
+function bfs2(graph, startNode, targetNode, position) {
+    const queue = [startNode];
+    const visited = new Set();
+    const parent = {};
+
+    console.log(startNode)
+
+
+    let counter = 0; 
+    let tokenCounter = 0;
+    while (queue.length > 0) {
+        counter++;
+        if(counter > 100)
+            return;
+
+        const currentNode = queue.shift();
+        visited.add(currentNode);
+
+        console.log("Node: ", currentNode);
+        
+        if (currentNode === targetNode) {
+            console.log("FOUND!!!")
+            return getPath(startNode, targetNode, parent);
+        }
+
+        if(currentNode.startsWith("T_"))
+            continue;
+
+        //Each metanode contains nodes. This double for loop just adds all nodes in the queue.
+        let edges = graph[currentNode];
+
+        if(!edges)  
+            continue;
+
+        for (let edgeIndex = 0; edgeIndex < edges.length; edgeIndex++) {
+            let metaNode = edges[edgeIndex];
+            for (let nodeIndex = 0; nodeIndex < metaNode.length; nodeIndex++) {
+                let node = metaNode[nodeIndex]
+                //add each node to the queue
+                queue.push(node);
+
+            }
+        }
+
+    }
+    return null; // Path not found
+}
+
+// function getPath(startNode, targetNode, parent) {
+//     const path = [targetNode];
+//     let current = targetNode;
+
+//     while (current !== startNode) {
+//         if (!parent[current]) {
+//             return null; // Target node is not reachable from start node
+//         }
+//         current = parent[current];
+//         path.unshift(current);
+//     }
+
+//     return path;
+// }
+
+// Example usage:
+const startNode2 = "PresentSimple";
+const targetNode2 = "T_Subject";
+let position2 = 1 //the position of the token we are looking for
+
+const path2 = bfs(dict_presentSimple, startNode2, targetNode2, position);
+console.log("Optimal Path:", path2);
